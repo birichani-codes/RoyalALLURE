f2b44f82871e6ce1fef54e397d1665af81485d40
birichani_codes <birichani25@gmail.com>
2025-02-21 12:27:59 +0300

    added about us , added more on homepage and watches section

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/watches', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'watches.html'));
+});
+app.get('/about-us', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about-us.html'));
+});
+
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
